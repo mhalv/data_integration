b946a4cdbc5c2e4e740b82678b65c576deafb5e5
almartin82 <almartin@gmail.com>
2014-11-18 10:58:12 -0500

    recursion is hard

--- khan/pull_skill_metadata.py
+++ khan/pull_skill_metadata.py
@@ -4,25 +4,25 @@ import csv
 #csv format
 csv.register_dialect('ALM', delimiter=',', quoting=csv.QUOTE_ALL)
 
-url = 'http://www.khanacademy.org/api/v1/topictree'
-#url = 'http://www.khanacademy.org/api/v1/topic/math'
+#url = 'http://www.khanacademy.org/api/v1/topictree'
+url = 'http://www.khanacademy.org/api/v1/topic/math'
 #url = 'http://www.khanacademy.org/api/v1/topic/cc-eighth-grade-math'
 #url = 'http://www.khanacademy.org/api/v1/topic/cc-8th-numbers-operations'
 #url = 'http://www.khanacademy.org/api/v1/topic/basic-geo-special-right-triangle'
 
-foo = requests.get(url).json()
+khan_tree = requests.get(url).json()
 
 #globals
 global_depth = 0
 
 skill = {
-    'd_0': None,
-    'd_1': None,
-    'd_2': None,
-    'd_3': None,
-    'd_4': None,
-    'd_5': None,
-    'd_6': None,
+    0: None,
+    1: None,
+    2: None,
+    3: None,
+    4: None,
+    5: None,
+    6: None,
     'domain_slug': None,
     'hide_meta': None,
     'description': None,
@@ -30,59 +30,63 @@ skill = {
     'deleted': None
 }
 
-def parse_topics(tree):
-    print 'CALLED, w/ ' + tree['title']
+def parse_topics(tree, parent='start'):
+    print 'CALLED, w/ ' + tree['title'] + ' and parent ' + parent
 
-    global global_depth, skill
-    skill['d_'+ str(global_depth)] = tree['node_slug']
+    global skill
+    #handle init
+    if parent=='start':
+        skill[0] = tree['node_slug']
     skill['hide_meta'] = tree['hide']
     skill['domain_slug'] = tree['domain_slug']
     skill['description'] = tree['description']
     skill['in_knowledge_map'] = tree['in_knowledge_map']
     skill['deleted'] = tree['deleted']
 
-    if 'children' in tree.keys():
-        #print "\t\t...with children. iterating..."
+    for i in tree['children']:
+        #if the child is a TOPIC, unpack it.
+        #global_depth += 1
 
-        for i in tree['children']:
-            #if the child is a TOPIC, unpack it.
-            global_depth += 1
+        #can we just track PARENT name here?
+        parent = tree['node_slug']
+        #print '\tparent is ' + parent
 
-            if i['kind'] == u'Topic':
-                #print '\t\t\tchild is a topic.  unpack it'
-                child_url = 'http://www.khanacademy.org/api/v1/topic/' + i['node_slug']
-                #print '\t\t\t' + child_url
-                child_tree = requests.get(child_url).json()
+        if i['kind'] == u'Topic':
+            #print '\t\t\tchild is a topic.  unpack it'
+            child_url = 'http://www.khanacademy.org/api/v1/topic/' + i['node_slug']
+            #print '\t\t\t' + child_url
+            child_tree = requests.get(child_url).json()
 
-                #update the globals
-                skill['d_'+ str(global_depth)] = i['node_slug']
-                skill['hide_meta'] = i['hide']
+            #update the globals
+            skill['hide_meta'] = i['hide']
 
-                parse_topics(child_tree)
+            for n in range(0, 6):
+                if skill[n] == parent:
+                    skill[n+1] = i['node_slug']
 
-            #otherwise, record it!
-            else:
-                #print '\t\t\tchild is NOT a topic.  Parse it!'
+            parse_topics(child_tree, parent)
 
-                #clean out anything bigger than global depth before calling
-                for n in range(0, 6):
-                    if n > global_depth:
-                        skill['d_'+ str(n)] = None
+        #otherwise, record it!
+        else:
+            #print '\t\t\tchild is NOT a topic.  Parse it!'
 
-                #print skill
+            #find where we are in the tree and record it
+            # for n in range(0, 6):
+            #     if skill[n] == parent:
+            #         skill[n+1] == i['node_slug']
 
-                record_skill_props(
-                  skill_meta=skill,
-                  skill_dict=i
-                )
-                global_depth = 0
-    else:
-        #print "\t\t...withOUT children."
-        child_url = 'http://www.khanacademy.org/api/v1/topic/' + tree['node_slug']
-        child_tree = requests.get(child_url).json()
-        global_depth += 1
-        parse_topics(child_tree)
+            #clean out anything bigger than global depth before calling
+            # for n in range(0, 6):
+            #     if n > global_depth:
+            #         skill[n] = None
 
+            #print skill
+
+            record_skill_props(
+              skill_meta=skill,
+              skill_dict=i
+            )
+#            global_depth = 0
 
 def record_skill_props(skill_meta, skill_dict):
     #everything from meta, that tracks the parent levels
@@ -107,7 +111,7 @@ def record_skill_props(skill_meta, skill_dict):
     return None
 
 master_skills = []
-parse_topics(foo)
+parse_topics(khan_tree)
 
 #print master_skills
 
